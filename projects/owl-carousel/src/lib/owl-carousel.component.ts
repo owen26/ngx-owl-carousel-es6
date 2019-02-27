fb3db45bea01302629e5f5d45ab70fbb86aa90a7
xinan@tn <xinan@touchnote.com>
2019-02-27 17:17:39 +0000

    skeleton

--- projects/owl-carousel/src/lib/owl-carousel.component.ts
+++ projects/owl-carousel/src/lib/owl-carousel.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ElementRef,
+  ViewEncapsulation
+} from '@angular/core';
+import * as jQuery from 'jquery';
+import 'owl.carousel.es6';
 
 @Component({
   selector: 'owl-carousel',
-  template: `
-    <p>
-      owl-carousel works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './owl-carousel.component.html',
+  styleUrls: ['./owl-carousel.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class OwlCarouselComponent implements OnInit {
+export class OwlCarouselComponent implements OnInit, AfterViewInit {
+  constructor(private el: ElementRef) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const $ele = jQuery(this.el.nativeElement).find('.owl-carousel') as any;
+
+    $ele.owlCarousel({
+      loop: true,
+      items: 1,
+      autoplay: true
+    });
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OwlCarouselModule } from 'owl-carousel-es6';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, OwlCarouselModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
